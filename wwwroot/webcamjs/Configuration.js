3b2413a1af63e64745b2201a1cdffc2fc5f81131
bharathwajv <71205976+bharathwajv@users.noreply.github.com>
2021-04-16 09:47:12 +0530

    Changes
    Increased image res, SignalR changes

--- wwwroot/webcamjs/Configuration.js
+++ wwwroot/webcamjs/Configuration.js
@@ -1,11 +1,15 @@
-﻿function ready() {
+﻿var data = null;
+
+function ready() {
 	if (document.readyState == 'complete') {
 		Webcam.set({
-			width: 320,
-			height: 240,
-			image_format: 'jpeg',
-			jpeg_quality: 90,
-		});
+			width: 1280, // dest
+			height: 720,
+			width: 2560, // live preview size
+			height: 1960,
+            image_format: 'jpeg',
+            jpeg_quality: 100
+});
 		try {
 			Webcam.attach('#camera');
 		} catch (e) {
@@ -14,15 +18,19 @@
 	}
 }
 
-function take_snapshot() {
-	// take snapshot and get image data
-	var data = null;
-	Webcam.snap(function (data_uri) {
-
-		data = data_uri;
-	});
+function snap() {
 	return data;
-	console.log(data);
+}
+window.take_snapshot = function() {
+	// take snapshot and get image data
+	try {
+		Webcam.snap(function (data_uri) {
+			data = data_uri;
+			document.getElementById("result").innerHTML = '<img id = "captured_image" src="' + data_uri + '"/>'
+		});
+	} catch (e) {
+	}
+	
 }
 window.Alert = function (message) {
 	alert(message);
